bf9defa8a0bc8b309443f091f0288cede2d7cf7f
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-07 21:16:48 +0530

    dg

--- terraform project/main.tf
+++ terraform project/main.tf
@@ -56,8 +56,22 @@ resource "aws_security_group" "example" {
 }
 
 resource "aws_instance" "ec2" {
-  ami                    = "ami-03d6d8b8d825259e5"
+  ami                    = "ami-073f8d21389408eb4"
   instance_type          = "t2.micro"
   key_name               = "ec2-key"
   vpc_security_group_ids = [aws_security_group.example.id]
-}
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo bash /root/apache/bin/catalina.sh start"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "ec2-key"
+      host        = self.public_ip
+    }
+  }
+    
+
