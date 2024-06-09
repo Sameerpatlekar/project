c478f9fbf88cbcd0d76c02423f527eede0071e58
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-09 14:51:04 +0530

    cb

--- terraform project/module/rds/main.tf
+++ terraform project/module/rds/main.tf
@@ -10,7 +10,7 @@ resource "aws_db_instance" "rds" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = var.db_instance_class
-  name                 = var.db_name
+  db_name                 = var.db_name
   username             = var.db_username
   password             = var.db_password
   parameter_group_name = "default.mysql8.0"
