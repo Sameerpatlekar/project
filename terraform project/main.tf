2472e3efa345f1b6fe6909dd57a081ac5d49e9fa
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-09 23:53:32 +0530

    fh

--- terraform project/main.tf
+++ terraform project/main.tf
@@ -2,6 +2,58 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+resource "aws_security_group" "example" {
+  name        = "example-security-group"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+        from_port   = 3306
+        to_port     = 3306
+        protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # This means all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example-security-group"
+  }
+}
+
 resource "aws_instance" "ec2" {
   ami                    = "ami-0f0efa3f97e6a443f"
   instance_type          = "t2.micro"
