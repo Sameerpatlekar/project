078861c9251a6160e5fb96b6d9ac509364ef6fad
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-09 23:55:36 +0530

    fh

--- terraform project/main.tf
+++ terraform project/main.tf
@@ -74,5 +74,9 @@ resource "aws_instance" "ec2" {
   }
 }   
 
+variable "private_key_path" {
+  description = "Path to the private key for SSH access"
+  default     = "/home/sameer/.ssh/id_rsa"  # Adjust this path to match your private key's location
+}
 
 
