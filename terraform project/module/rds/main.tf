27f86f9a5d5b9ad7d3054905b534b5f38ebc95e5
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-09 14:39:31 +0530

    cfg

--- terraform project/module/rds/main.tf
+++ terraform project/module/rds/main.tf
@@ -1,71 +1,38 @@
+# main.tf
+
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
-resource "aws_security_group" "example" {
-  name        = "example-security-group"
-  description = "Allow SSH and HTTP traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-    ingress {
-        from_port   = 3306
-        to_port     = 3306
-        protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # This means all protocols
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "example-security-group"
-  }
-}
-
-resource "aws_db_instance" "mydb" {
-  allocated_storage    = 20
+resource "aws_db_instance" "example" {
+  identifier          = "free-tier-db-instance"
+  allocated_storage    = var.db_allocated_storage
   engine               = "mysql"
   engine_version       = "8.0"
-  instance_class       = "db.t2.micro"
+  instance_class       = var.db_instance_class
+  name                 = var.db_name
   username             = var.db_username
   password             = var.db_password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
-  publicly_accessible  = false
-  vpc_security_group_ids = [aws_security_group.example.id] # Replace with your security group ID
-  db_subnet_group_name   = aws_db_subnet_group.mydb_subnet_group.name
-
-  tags = {
-    Name = "MyDB"
-  }
 }
 
 
-resource "null_resource" "initialize_db" {
-  provisioner "local-exec" {
-    command = <<EOT
-    mysql -h ${aws_db_instance.mydb.endpoint} -u ${var.db_username} -p${var.db_password} -e "
-   CREATE TABLE if not exists students(student_id INT NOT NULL AUTO_INCREMENT,
-	student_name VARCHAR(100) NOT NULL,
-    student_addr VARCHAR(100) NOT NULL,
-	student_age VARCHAR(3) NOT NULL,
-	student_qual VARCHAR(20) NOT NULL,
-	student_percent VARCHAR(10) NOT NULL,
-	student_year_passed VARCHAR(10) NOT NULL,
-	PRIMARY KEY (student_id)
-     
-    );
-    "
-    EOT
+resource "aws_security_group" "example" {
+  name        = "example-sg"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  depends_on = [aws_db_instance.mydb]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- terraform project/module/rds/variable.tf
+++ terraform project/module/rds/variable.tf
@@ -1,42 +1,26 @@
-variable "region" {
-  description = "The AWS region to deploy in"
-  default     = "us-east-1"
-}
+# variables.tf
 
-variable "vpc_id" {
-  description = "The VPC ID to deploy into"
-}
-
-variable "subnet_ids" {
-  description = "A list of subnet IDs to deploy into"
-  type        = list(string)
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"
-}
-
-variable "key_name" {
-  description = "The key pair name to use for EC2 instances"
-}
-
-variable "private_key_path" {
-  description = "Path to the private key for SSH access"
+variable "db_name" {
+  default = "my-database"
 }
 
 variable "db_username" {
-  description = "The database admin username"
   default = "admin"
 }
 
 variable "db_password" {
-  description = "The database admin password"
-  sensitive   = true
   default = "admin123"
+  sensitive = true
 }
 
-variable "db_name" {
-  description = "The database name"
-  default     = "myrds"
+variable "db_allocated_storage" {
+  default = 20
+}
+
+variable "db_instance_class" {
+  default = "db.t2.micro"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
 }
