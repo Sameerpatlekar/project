d6158620fb12177b72bdd1121423990c93b91583
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-09 15:30:35 +0530

    fghf

--- terraform project/module/rds/main.tf
+++ terraform project/module/rds/main.tf
@@ -15,6 +15,7 @@ resource "aws_db_instance" "rds" {
   password             = var.db_password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
+  vpc_security_group_ids = [aws_security_group.sg.id]
 }
 
 
