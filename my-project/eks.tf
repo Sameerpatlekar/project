e55fdc1ef65e18ff681b26d5d2b36c0c78210113
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-12 00:19:53 +0530

    gj

--- my-project/eks.tf
+++ my-project/eks.tf
@@ -12,7 +12,7 @@ resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "my-eks-node-group"
   node_role_arn   = "arn:aws:iam::712340109036:role/node-ec2-access"
-  subnet_ids      = subnet_ids = [aws_subnet.main["subnet-07ee43f167f3e612c"].id, aws_subnet.main["subnet-0ccfa04a7628f349a"].id]
+  subnet_ids      =  [aws_subnet.main["subnet-07ee43f167f3e612c"].id, aws_subnet.main["subnet-0ccfa04a7628f349a"].id]
 
 
   scaling_config {
