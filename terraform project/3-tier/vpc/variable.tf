c61af42f368e8b53b74487d44884f6839858c416
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-12 16:53:10 +0530

    gff

--- terraform project/3-tier/vpc/variable.tf
+++ terraform project/3-tier/vpc/variable.tf
@@ -11,7 +11,10 @@ variable "environment" {
 }
 
 variable "public_subnets_cidr" {
-  default = "10.0.0.0/24"
+  default = "10.0.1.0/24"
+}
+variable "private_subnets_cidr" {
+  default = "10.0.2.0/24"
 }
 
 variable "public_availability_zones" {
