adec43f3e725f5654946dddc2738739a21c0305f
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-12 23:40:01 +0530

    fh

--- terraform project/3-tier/vpc/main.tf
+++ terraform project/3-tier/vpc/main.tf
@@ -1,26 +1,28 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "main" {
+
   cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "$(var.environment)-vpc"
+    Name        = "${var.environment}-vpc"
     Environment = var.environment
   }
 }
 
 resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnets_cidr
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnets_cidr
   map_public_ip_on_launch = true
-  availability_zone = var.public_availability_zones
+  availability_zone       = var.public_availability_zones
 
   tags = {
-    Name = "$(var.environment)-public-subnet"
+    Name        = "${var.environment}-public-subnet"
     Environment = var.environment
   }
+
 }
 
 resource "aws_subnet" "private" {
@@ -29,19 +31,23 @@ resource "aws_subnet" "private" {
   availability_zone = var.private_availability_zones
 
   tags = {
-    Name = "$(var.environment)-private-subnet"
+    Name        = "${var.environment}-private-subnet"
     Environment = var.environment
   }
+
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
 }
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "$(var.environment)-igw"
+    Name        = "${var.environment}-igw"
     Environment = var.environment
   }
-}
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -50,12 +56,12 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
+
   tags = {
-    Name = "$(var.environment)-public-route-table"
+    Name        = "${var.environment}-public-route-table"
     Environment = var.environment
   }
 }
-
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
@@ -65,7 +71,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "$(var.environment)-private-route-table"
+    Name        = "${var.environment}-private-route-table"
     Environment = var.environment
   }
 
