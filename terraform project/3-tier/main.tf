78e1a4856a0b8a258432054ab19e8db5b892c032
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-13 17:03:57 +0530

    fhfh

--- terraform project/3-tier/main.tf
+++ terraform project/3-tier/main.tf
@@ -4,4 +4,11 @@ provider "aws" {
 
 module "vpc" {
   source = "./vpc" 
+  aws_region = "ap-south-1"
+  vpc_cidr = "192.168.0.0/26"
+  environment = "Networking"
+  public_subnets_cidr = "192.168.0.16/28"
+  private_subnets_cidr = "192.168.0.32/28"
+  public_availability_zones = "ap-south-1a"
+  private_availability_zones = "ap-south-1b"
 }
