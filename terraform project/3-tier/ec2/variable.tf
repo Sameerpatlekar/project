362a83c10a4eb529b347330873a44cb2894c164d
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-13 23:40:19 +0530

    dg

---
+++ terraform project/3-tier/ec2/variable.tf
@@ -0,0 +1,30 @@
+variable "ami" {
+    description = "image of instance"
+    type = string
+}
+
+variable "instance_type" {
+    description = "instance type of instance"
+    type = string
+}
+
+variable "public_subnet_id" {
+    description = "In which subnet, instance is created"
+    type = string
+}
+
+variable "private_subnet_id" {
+    description = "In which subnet, instance is created"
+    type = string
+}
+
+variable "environment" {
+    description = "environment department"
+    type = string
+}
+
+variable "key_name" {
+    description = "key name "
+    type = string
+}
+
