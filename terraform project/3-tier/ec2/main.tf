c2dbc7f34e24af8274e6b88ee2f7186eec4c3554
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-14 01:14:23 +0530

    today's final commit

--- terraform project/3-tier/ec2/main.tf
+++ terraform project/3-tier/ec2/main.tf
@@ -15,6 +15,7 @@ resource "aws_instance" "backend" {
   instance_type = var.instance_type
   subnet_id     = var.private_subnet_id  # Assuming the subnet is provided in the VPC
   key_name      = var.key_name
+  vpc_security_group_ids = [var.sg_id]
   tags = {
     Name = "backend-instance"
     environment = var.environment
