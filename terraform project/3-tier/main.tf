d9180e1539f4aad31c38a2df82b6aa334edda794
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-15 01:56:57 +0530

    fhfh

--- terraform project/3-tier/main.tf
+++ terraform project/3-tier/main.tf
@@ -30,8 +30,8 @@ module "ec2" {
 resource "null_resource" "ansible_playbook" {
   provisioner "local-exec" {
     command = <<EOT
-      bash ${path.module}/generate_inventory.sh
-      ansible-playbook -i ${path.module}/inventory.ini ${path.module}/playbook.yml
+      bash generate_inventory.sh
+      ansible-playbook -i inventory.ini playbook.yml
     EOT
   }
 
