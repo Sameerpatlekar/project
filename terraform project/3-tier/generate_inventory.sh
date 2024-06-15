c02de55a64935d140add6f21382fa1207b57d451
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-15 23:47:48 +0530

    fdh

--- terraform project/3-tier/generate_inventory.sh
+++ terraform project/3-tier/generate_inventory.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Extract the outputs from Terraform
-PUBLIC_IP=$(terraform output -raw module.ec2.public_instance_public_ip)
-PRIVATE_IP=$(terraform output -raw module.ec2.private_instance_private_ip)
+PUBLIC_IP=$(terraform output -raw public_instance_public_ip)
+PRIVATE_IP=$(terraform output -raw private_instance_private_ip)
 
 # Create the Ansible inventory file
 cat > inventory.ini <<EOF
 [public]
-public_instance ansible_host=${PUBLIC_IP} ansible_user=ubuntu ansible_ssh_private_key_file=/home/sameer/.ssh/my-key.pem
+public_instance ansible_host=${PUBLIC_IP} ansible_user=ubuntu 
 
 [private]
-private-instance ansible_host=${PRIVATE_IP} ansible_user=ubuntu ansible_ssh_private_key_file=/home/sameer/.ssh/my-key.pem
+private-instance ansible_host=${PRIVATE_IP} ansible_user=ubuntu 
 
 [private:vars]
-ansible_ssh_common_args='-o ProxyCommand="ssh -W %h:%p -q ec2-user@${PUBLIC_IP} -i /home/sameer/.ssh/my-key.pem"'
+ansible_ssh_common_args='-o ProxyCommand="ssh -W %h:%p -q ubuntu@${PUBLIC_IP} '
 
 EOF
