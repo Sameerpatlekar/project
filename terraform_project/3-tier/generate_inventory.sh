2c6d4b8511378a84f2c1eed83313b6d85ab68d95
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-17 15:32:28 +0530

    bjb

--- terraform_project/3-tier/generate_inventory.sh
+++ terraform_project/3-tier/generate_inventory.sh
@@ -6,11 +6,14 @@ PRIVATE_IP=$(terraform output -raw private_instance_private_ip)
 
 # Create the Ansible inventory file
 cat > inventory.ini <<EOF
-[public]
+[bastion]
 public_instance ansible_host=${PUBLIC_IP} ansible_user=ubuntu ansible_ssh_private_key_file=/home/sameer/.ssh/my-key.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 
 [private]
-private-instance ansible_host=${PRIVATE_IP} ansible_user=ubuntu ansible_ssh_private_key_file=/home/sameer/.ssh/my-key.pem ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+private-instance ansible_host=${PRIVATE_IP} 
+ansible_user=ubuntu 
+ansible_ssh_private_key_file=/home/sameer/.ssh/my-key.pem 
+ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 
 [private:vars]
 ansible_ssh_common_args='-o ProxyCommand="ssh -W %h:%p -q ubuntu@${PUBLIC_IP}"' ansible_ssh_private_key_file=/home/sameer/.ssh/my-key.pem  ansible_ssh_common_args='-o StrictHostKeyChecking=no'
