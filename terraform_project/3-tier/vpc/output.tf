4e4f1be8e19cd3f056038189e49a1fd2ceb11a2e
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-27 21:10:18 +0530

    ffhhgj

--- terraform_project/3-tier/vpc/output.tf
+++ terraform_project/3-tier/vpc/output.tf
@@ -5,10 +5,10 @@ output "vpc_id" {
 
 output "public_subnet_id" {
     description = "the id of the public subnet"
-    value = aws_subnet.public.id
+    value = aws_subnet.public[count.index].id
 }
 
 output "private_subnet_id" {
     description = "the id of the private subnet"
-    value = aws_subnet.private.id
+    value = aws_subnet.private[count.index].id
 }
