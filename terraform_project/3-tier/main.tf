387fed06ce7507ee78796be4f027e30f956f0780
Sameerpatlekar <sameerpatlekar@gmail.com>
2024-07-05 01:19:36 +0530

    updated

--- terraform_project/3-tier/main.tf
+++ terraform_project/3-tier/main.tf
@@ -56,7 +56,7 @@ resource "null_resource" "output_value" {
   provisioner "local-exec" {
     command = "terraform output -json > terraform_outputs.json "
   }
-  depends_on = [module.rds.rds_endpoint , module.alb.load_balancer_dns]
+  depends_on = [null_resource.nginx_setup_onprivate]
 }
 
 resource "null_resource" "create_database" {
@@ -81,7 +81,7 @@ resource "null_resource" "nginx_setup" {
 
 resource "null_resource" "nginx_setup_onprivate" {
   provisioner "local-exec" {
-    command = "ansible-playbook -i inventory.ini nginx_setup_onprivate.yml"
+    command = "bash ${path.module}/generate_inventory.sh && ansible-playbook -i inventory.ini nginx_setup_onprivate.yml"
   }
-  depends_on = [null_resource.script_file]
+  depends_on = [module.rds , module.alb]
 }
