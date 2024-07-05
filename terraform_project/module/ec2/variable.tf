4527e3dc44e9a05ba750bef70d0155e5797debc9
root <root@LAPTOP-P0Q8LJP8>
2024-07-05 14:17:57 +0530

    sddf

--- terraform_project/module/ec2/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "private_key_path" {
-  description = "Path to the private key for SSH access"
-  default     = "/home/sameer/.ssh/id_rsa"  # Adjust this path to match your private key's location
-}
-
